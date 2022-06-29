acd6a9b97f58da43d5de4fd1d9aeeb42b8b37683
Tademan21 <tadessefufamom@gmail.com>
2022-06-29 09:03:53 +0300

    add

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-def pow(a, b):
-    """Return a to the power of b."""
-    return (a ** b)
-© 2022 GitHub, Inc.
-Footer navigation
-Term
+#!/usr/bin/env python3
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
