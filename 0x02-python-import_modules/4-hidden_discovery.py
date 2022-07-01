dcfc5902770b5d454494f2eafe4aedca8ffa6b4a
Tademan21 <tadessefufamom@gmail.com>
2022-06-30 22:53:09 -0700

    dis

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the value of a from variable_laod_5"""
-    from variable_load_5 import a
-    print(a)
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
