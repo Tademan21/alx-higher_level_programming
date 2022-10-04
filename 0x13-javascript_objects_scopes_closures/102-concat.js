181d28bd5ade49f9cba0e64d7beafe3eef66615c
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-10-04 14:34:04 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,6 @@
-t fs = require('fs');
+#!/usr/bin/node
+
+const fs = require('fs');
 
 const fileA = fs.readFileSync(process.argv[2], 'utf8');
 const fileB = fs.readFileSync(process.argv[3], 'utf8');
