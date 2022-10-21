28f614c99cdf970a42e00c0bb6a6b935d6f781d5
Tademan21 <Tade21@gmail.com>
2022-10-21 12:29:39 +0000

    rr

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+ https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    response = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
