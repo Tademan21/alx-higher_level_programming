7dc534b4cb70b64b1e44dd4cc9c736d24da4ec67
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-10-21 15:35:38 +0300

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
-
-n script that takes in a URL, sends a request
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request
 to the URL and displays the body of the response."""
 
 import requests
