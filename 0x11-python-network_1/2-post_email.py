16314bce61ff3d5098a99ccdc42e1b80d614dca7
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-10-21 15:20:28 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
-
-n script that takes in a URLand an email, sends a POST
+#!/usr/bin/python3
+"""Python script that takes in a URLand an email, sends a POST
 request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)"""
 
