3c7e880f07314c015dc86b764d654274b4b763d6
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-10-21 15:24:56 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
-
-n script that takes in a URL, sends a request to the URL
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
 from urllib import request, error
